5b302b994ee9d4442ac8162d983d7d2bd142a376
mo <meganannetteolson@yahoo.com>
2017-08-25 15:11:03 -0700

    change to one single form

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,56 @@
 // Business Logic
 
-function Pizza (size, base, toppings) {
+function Pizza (size, cheese, sauce, topping) {
   this.size = size;
-  this.base = base;
-  this.toppings = toppings;
-  this.total = 0;
+  this.cheese = cheese;
+  this.sauce = sauce;
+  this.topping = topping;
+  this.price;
 }
-Pizza.prototype.sizeCost = function () {
-  if (this.size = "small") {
-    this.total += 5;
-  } else if (this.size = "medium") {
-    this.total += 7;
-  } else {
-    this.total += 11;
+Pizza.prototype.findPrice = function (size) {
+  this.price = 5;
+  if (this.size === "Small") {
+    this.total += 2;
+  } else if (this.size === "Medium") {
+    this.price += 4;
+  } else if (this.size === "Large"){
+    this.price += 6;
+  } if (this.crust === "Classic Crust") {
+    this.price += 0;
+  } else if (this.crust === "Thin Crust"){
+    this.price -= 1;
+  } if (this.cheese === "No Cheese") {
+    this.price -= 1;
+  } else if (this.cheese === "Mozzerella" || this.cheese === "Cheddar") {
+    this.price += 0;
+  } for (var i = 0; i < this.toppings.length; i++) {
+    this.price += 1;
   }
+  console.log(this.price);
 }
-// Pizza.prototype.
+// Pizza.protoype.order = function () {
+//   return "Your order for a " + this.size + " pizza with a " + this.crust + " with " + this.sauce + " and " + this.cheese + " topped with " + this.toppings + " is " + this.price;
+// }
+// var display = function() {
+//
+// }
 
 
 // User Interface Logic
 $(document).ready(function(){
-  $("#choose-size").click(function(e){
+  $("#order").click(function(e){
     e.preventDefault();
     var size = $("#chooseSize").val();
-    $("#base").show();
-    $("#size").hide();
-  });
-  $("#choose-base").click(function(e){
-    e.preventDefault();
-    $("#toppings").show();
-    $("#base").hide();
     var crust = $("#crust").val();
     var sauce = $("#sauce").val();
     var cheese = $("#cheese").val();
-  });
-  $("#showTotal").click(function(e){
-    e.preventDefault();
-    $("#total").show();
-    $("#toppings").hide();
-    $("input:checkbox[name=topping]:checked").each(function(){
-      var toppingsTotal = $(this).val();
-    });
+    var topping = $("input:checkbox[name=topping]:checked").map(function(){
+
+    var newPizza = new Pizza(size, crust, sauce, cheese, topping);
+    // $("#showTotal").click(function(e){
+    //   e.preventDefault();
+      // $("#total").show();
+      // $("#toppings").text(newPizza.topping.join(", "));
+    // });
   });
 });
